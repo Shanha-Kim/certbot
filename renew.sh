3c795f809e806d9135598dcff99bc77a90394bde
Shanha-Kim <shk8532@gmail.com>
2024-04-15 22:25:51 +0900

    update auto renewal

--- renew.sh
+++ renew.sh
@@ -1,3 +1,15 @@
-docker run --rm --name certbot -v '/home/tlexibetarelease/tlexi/certbot/certbot/conf:/etc/letsencrypt' -v '/home/tlexibetarelease/tlexi/certbot/certbot/logs:/var/log/letsencrypt' -v '/home/tlexibetarelease/tlexi/certbot/certbot/data:/var/www/certbot' certbot/certbot renew --server https://acme-v02.api.letsencrypt.org/directory --cert-name tlexi.com
+docker run --rm --name certbot \
+ -v '/home/user1/tlexi/certbot/certbot/conf:/etc/letsencrypt' \
+ -v '/home/user1/tlexi/certbot/certbot/logs:/var/log/letsencrypt' \
+ -v '/home/user1/tlexi/certbot/certbot/data:/var/www/certbot' \
+ certbot/certbot renew --server https://acme-v02.api.letsencrypt.org/directory --cert-name tlexi.com
 
-docker run  -it --rm --name certbot -v '/home/tlexibetarelease/tlexi/certbot/certbot/conf:/etc/letsencrypt' -v '/home/tlexibetarelease/tlexi/certbot/certbot/logs:/var/log/letsencrypt' -v '/home/tlexibetarelease/tlexi/certbot/certbot/data:/var/www/certbot' certbot/certbot certificates
+docker run  -it --rm --name certbot \
+ -v '/home/user1/tlexi/certbot/certbot/conf:/etc/letsencrypt' \
+ -v '/home/user1/tlexi/certbot/certbot/logs:/var/log/letsencrypt' \
+ -v '/home/user1/tlexi/certbot/certbot/data:/var/www/certbot' \
+ certbot/certbot certificates
+
+  bash run_server.sh
+
+#   crontab save : 0 19 1 * * /home/user1/tlexi/backend/ssl_renew.sh >> /home/user1/tlexi/backend/log/ssl_renew.log
